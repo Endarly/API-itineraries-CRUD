8c6aa219daca112229ad3bc1d33cac43447fb58e
endarly <endarly@gmail.com>
2023-07-11 18:08:06 -0300

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 require("dotenv").config()
 require('./config/database')
 const Routes = require('./routes/routes')
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 4001;
 
 app.set("port", PORT);
 
+//middlewares
+app.use(cors())
 app.use(express.json())
 app.use('/api', Routes)
 
